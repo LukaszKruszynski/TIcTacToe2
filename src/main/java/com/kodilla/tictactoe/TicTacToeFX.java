fc86db1a7e4b1374d99ec8aeecb400c42f17d9ad
LukaszKruszynski <lukaszkruszynski96@gmail.com>
2020-05-01 14:22:36 +0200

    javaFX in progress

--- src/main/java/com/kodilla/tictactoe/TicTacToeFX.java
+++ src/main/java/com/kodilla/tictactoe/TicTacToeFX.java
@@ -7,6 +7,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 
 public class TicTacToeFX extends Application {
@@ -15,6 +16,14 @@ public class TicTacToeFX extends Application {
     private Image imageOShape = new Image("file:src/main/resources/oShape.png");
     private Image imageXShape = new Image("file:src/main/resources/xShape.png");
     private Image imageNoneShape = new Image("file:src/main/resources/noneShape.png");
+
+    public void setShape(int x, int y, Image imageNoneShape) {
+        for(int row = 0; row < 3; row++) {
+            for(int col = 0; col < 3; col++) {
+                setShape(row,col,imageNoneShape);
+            }
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
@@ -40,16 +49,13 @@ public class TicTacToeFX extends Application {
         ImageView imageViewNoneShapeCol2Row1 = new ImageView(imageNoneShape);
         ImageView imageViewNoneShapeCol2Row2 = new ImageView(imageNoneShape);
 
+        ColumnConstraints col0 = new ColumnConstraints(140);
+        ColumnConstraints col1 = new ColumnConstraints(140);
+        ColumnConstraints col2 = new ColumnConstraints(140);
 
-
-
-        ColumnConstraints col0 = new ColumnConstraints(130);
-        ColumnConstraints col1 = new ColumnConstraints(130);
-        ColumnConstraints col2 = new ColumnConstraints(130);
-
-        RowConstraints row0 = new RowConstraints(130);
-        RowConstraints row1 = new RowConstraints(130);
-        RowConstraints row2 = new RowConstraints(130);
+        RowConstraints row0 = new RowConstraints(140);
+        RowConstraints row1 = new RowConstraints(140);
+        RowConstraints row2 = new RowConstraints(140);
 
         GridPane gridPane = new GridPane();
 
@@ -66,11 +72,12 @@ public class TicTacToeFX extends Application {
         gridPane.add(imageViewNoneShapeCol2Row0,2,0);
         gridPane.add(imageViewNoneShapeCol2Row1,2,1);
         gridPane.add(imageViewNoneShapeCol2Row2,2,2);
+        gridPane.add(imageViewXShape,1,2);
+        gridPane.add(imageViewOShape,0,0);
+
 
         gridPane.setVgap(5);
         gridPane.setHgap(5);
-        gridPane.setPadding(new Insets(5));
-
 
         gridPane.setBackground(background);
         Scene scene = new Scene(gridPane, 420, 420, Color.GOLDENROD);
